b450e8acfaaf4bf74259b25768a393591e5459a8
daiyu <1435480344@qq.com>
2019-06-11 12:48:36 +0800

    web商城提交

---
+++ src/com/dao/UserDao.java
@@ -0,0 +1,15 @@
+package com.dao;
+
+import com.domain.User;
+
+public interface UserDao {
+
+	void save(User user) throws Exception;
+
+	User getByCode(String code) throws Exception;
+
+	void update(User user) throws Exception;
+
+	User getByUsernameAndPwd(String username, String password) throws Exception;
+
+}
